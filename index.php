45d84c0076ae41e758171fa4b2230b977ee32dde
Laura Campo <lauracampodeveloper@gmail.com>
2018-10-30 11:28:07 +0100

    Millores de maquetació
    Estil donat a algunes caixes.

--- index.php
+++ index.php
@@ -4,12 +4,18 @@
         <meta charset="UTF-8">
         <title>Estalviar i Més</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
         <link rel="stylesheet" href="style.css" type="text/css">
         <script src="jQuery_3.3.1.js"></script>
         <script type="text/javascript" src="scripts.js"></script>
     </head>
     <body>
         <header> <!-- capcelera central superior: data actual + safata de notificacions -->
+            <div>
+                <a href="index.php">
+                    <img id="logo_sup" src="images/porquet_logo.png" alt="logo">
+                </a>
+	    </div>
             <div id="notifications">
             <div id="current_date">
                 <script>
@@ -20,16 +26,11 @@
                 </script>
                 </div>
             <div id="notification_reminder">
-                <p>No té missatges per avui</p>
+                <p>No tens missatges per avui</p>
             </div>
         </div>
         </header>
-        <aside> <!-- menú principal, a capa lateral -->
-		<div>
-                    <a href="index.php">
-                        <img id="logo_sup" src="imagenes/taxilogo.png" alt="logo taxi">
-                    </a>
-		</div> 
+        <aside> <!-- menú principal, a capa lateral --> 
 			<nav>
 			<!-- menú lateral (principal) -->
 			    <ul id="lista_menu">
